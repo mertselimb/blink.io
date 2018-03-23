eea54bc7c69129404c1c9548ff4c50cd5a4d2f01
Mert Selimbeyoğlu <mertselimb@users.noreply.github.com>
2018-03-23 14:52:20 +0300

    finish starter code

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 var express = require('express')
-var app = express()
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get('/', function (req, res) {
     res.render("index")
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+http.listen(9999, () => console.log('Blink.io listening on port 9999!'));
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+    socket.on('message', function (msg) {
+        console.log('message: ' + msg);
+    });
+});
