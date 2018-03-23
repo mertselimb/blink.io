d4c672aa962a89c1d0618421b2f8ba9cd925cfbd
Mert Selimbeyoğlu <mertselimb@users.noreply.github.com>
2018-03-23 08:28:43 +0300

    starter code

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express()
+
+app.set("view engine", "ejs");
+
+app.get('/', function (req, res) {
+    res.render("index")
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
