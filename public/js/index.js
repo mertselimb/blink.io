c1212324a68e458fa22a4eea616ffe82683e957b
Mert Selimbeyoğlu <mertselimb@users.noreply.github.com>
2018-03-31 14:55:56 +0300

    With scroll but no buttons

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,1 @@
 var socket = io();
-
-var amount = '';
-
-function scroll() {
-    $('.scrollDown').animate({
-        scrollTop: amount
-    }, 100, 'linear', function () {
-        if (amount != '') {
-            scroll();
-        }
-    });
-}
-$('.scrollDown').hover(function () {
-    amount = '+=20';
-    scroll();
-});
