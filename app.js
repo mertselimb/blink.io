76a1631e34937d841cffba6053608090610e2de8
Mert Selimbeyoğlu <mertselimb@users.noreply.github.com>
2018-04-07 22:31:58 +0300

    nearly end backend

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express')
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
+var io = require('socket.io')(http, {
+    wsEngine: 'ws'
+});
+connections = [];
+users = [];
+usernames = [];
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -10,15 +14,33 @@ app.use(express.static("public"));
 app.get('/', function (req, res) {
     res.render("index")
 })
-
-http.listen(9999, () => console.log('Blink.io listening on port 9999!'));
+http.listen(process.env.PORT || 9999, () => console.log('Blink.io listening on port 9999!'));
 
 io.on('connection', function (socket) {
-    console.log('a user connected');
     socket.on('disconnect', function () {
-        console.log('user disconnected');
+        if (!socket.username) return;
+        usernames.splice(usernames.indexOf(socket.username), 1);
+        users.splice(socket.username, 1);
+        updateUsers();
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected:' + socket.username + ' >> ' + connections.length + " sockets connected.");
     });
-    socket.on('message', function (msg) {
-        console.log('message: ' + msg);
+    socket.on('login', function (data) {
+        socket.username = data;
+        connections.push(socket);
+        users[socket.username] = socket;
+        usernames.push(data);
+        updateUsers();
+        console.log('Connected:' + data + ' >> ' + connections.length + " sockets connected.");
+    });
+    socket.on('message', function (data) {
+        socket.broadcast.to(users[data.to].id).emit('message', data);
+    });
+    socket.on('messageAll', function (data) {
+        io.sockets.emit("messageAll", data);
     });
 });
+
+function updateUsers() {
+    io.sockets.emit("updateUsers", usernames);
+}
--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,118 @@
 var socket = io();
+username = "";
+users = new Array(100);
+
+$(document).ready(function () {
+
+    $(".btnSend").click(function () {
+        sendMessage();
+    });
+    $(".messageInput").keypress(function (e) {
+        var key = e.which;
+        if (key == 13) // the enter key code
+        {
+            sendMessage();
+            return false;
+        }
+    });
+    $(document).on('click', '#btnUsername', function () {
+        if ($("#usernameInput").val()) {
+            endIntro();
+            connect();
+        }
+    });
+    $(document).on('keypress', '#usernameInput', function (e) {
+        if (e.which == 13) {
+            if ($("#usernameInput").val()) {
+                endIntro();
+                connect();
+                return false;
+            }
+            return false;
+        }
+    });
+    $(".panel .info").click(function () {
+        sender = $(this).children('span.userName').text();
+        $("#currentUserSign").text(sender.substring(0, 2));
+        $("#currentUserName").text(sender);
+    });
+});
+
+socket.on("updateUsers", function (data) {
+    var html = "";
+    for (var i = 0; i < data.length; i++) {
+        html += createUser(data[i]);
+    }
+    $(".users").html(html);
+    $(".panel .info").click(function () {
+        sender = $(this).children('span.userName').text();
+        $("#currentUserSign").text(sender.substring(0, 2));
+        $("#currentUserName").text(sender);
+    });
+});
+
+socket.on("message", function (data) {
+    console.log(data);
+});
+
+socket.on("messageAll", function (data) {
+    console.log(data);
+});
+
+function endIntro(params) {
+    username = $("#usernameInput").val();
+    $(".intro").fadeOut(400, function () {
+        $(".container").fadeIn(400, function () {
+            $(".container").css("display", "flex");
+        });
+    });
+}
+
+function sendMessage() {
+    to = $("#currentUserName").text();
+    message = $(".messageInput").val();
+    if (!message || !to) return false;
+    if (to != "All Chat") {
+        socket.emit('message', {
+            message: message,
+            to: to,
+            from: username
+        });
+        if (!users[to]) {
+            users[to] = [];
+        }
+        users[to].push({
+            from: "you",
+            message: message
+        });
+        console.log(users);
+    } else {
+        socket.emit('messageAll', {
+            message: message,
+            to: to,
+            from: username
+        });
+    }
+    if (to != "All Chat") {
+        $(".messages").append(createMessageOut(message));
+    }
+    $(".messageInput").val("");
+}
+
+function connect() {
+    socket.emit('login', username);
+}
+
+function createUser(username) {
+    return '<div class = "info"><span class = "userSign">' + username.substring(0, 2) + '</span> <span class = "userName" >' + username + '</span> </div>';
+}
+
+function createMessageIn(message) {
+    var d = new Date();
+    return "<div class='message incoming'><p>" + message + "</p><span>" + d.getHours() + ":" + d.getMinutes() + "</span></div>"
+}
+
+function createMessageOut(message) {
+    var d = new Date();
+    return "<div class='message send'><p>" + message + "</p><span>" + d.getHours() + ":" + d.getMinutes() + "</span></div>"
+}
