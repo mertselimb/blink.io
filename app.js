db398ed51a4f1438b7d90bb747f424482cb50140
Mert Selimbeyoğlu <mertselimb@users.noreply.github.com>
2018-04-20 21:42:30 +0300

    end of the project

--- app.js
+++ app.js
@@ -1,47 +1,50 @@
-var express = require('express')
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http, {
-    wsEngine: 'ws'
+//Classic setup
+var express = require('express') //Router framework
+var app = express(); //Router framework
+var http = require('http').Server(app); //Server framework
+var io = require('socket.io')(http, { //Socket connection framework
+    wsEngine: 'ws' //A bug solver for socket io that slowed down app
 });
-connections = [];
-users = [];
-usernames = [];
+connections = []; //Raw connection data
+users = []; //Connection data with usernames
+usernames = []; //Username data
 
-app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.set("view engine", "ejs"); //Pointing out we will be using ejs not html (for no good reason)
+app.use(express.static("public")); //Public directory will be static
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res) { //Express set up for root route
     res.render("index")
 })
-http.listen(process.env.PORT || 9999, () => console.log('Blink.io listening on port 9999!'));
-setInterval(updateUsers, 15000);
+http.listen(process.env.PORT || 9999, () => console.log('Blink.io listening on port 9999!')); //Start express server
+setInterval(updateUsers, 15000); //Update user data of clients for no good reason
 
-io.on('connection', function (socket) {
-    socket.on('disconnect', function () {
-        if (!socket.username) return;
-        usernames.splice(usernames.indexOf(socket.username), 1);
-        users.splice(socket.username, 1);
-        connections.splice(connections.indexOf(socket), 1);
-        console.log(socket.ip + '  Disconnected:' + socket.username + ' >> ' + connections.length + " sockets connected.");
+io.on('connection', function (socket) { //When a client connects
+    socket.on('disconnect', function () { //When a client disconnects
+        if (!socket.username) return; //If user not signed in return so we will not get errors
+        usernames.splice(usernames.indexOf(socket.username), 1); //Get rid of user data
+        users.splice(socket.username, 1); //Get rid of user data
+        connections.splice(connections.indexOf(socket), 1); //Get rid of user data
+        console.log(socket.ip + '  Disconnected:' + socket.username + ' >> ' + connections.length + " sockets connected."); //Write console the data
     });
-    socket.on('login', function (data) {
-        socket.username = data;
-        socket.ip = socket.conn.transport.socket._socket.remoteAddress;
-        connections.push(socket);
-        users[socket.username] = socket;
-        usernames.push(data);
-        console.log(socket.ip + '  Connected:' + data + ' >> ' + connections.length + " sockets connected.");
+    socket.on('login', function (data) { //When a client logins
+        socket.username = data; //Retrieve socket username
+        if (!socket.ip) { //If there is no ip (If user reconnects it will have ip)
+            socket.ip = socket.request.connection.remoteAddress; //Get client ip
+        }
+        connections.push(socket); //Push raw data array
+        users[socket.username] = socket; //Push data
+        usernames.push(data); //Push username as a dictionary (string indexed array)
+        console.log(socket.ip + '  Connected:' + data + ' >> ' + connections.length + " sockets connected."); //Write console the data
     });
-    socket.on('message', function (data) {
-        if (data.to != socket.username)
-            socket.broadcast.to(users[data.to].id).emit('message', data);
+    socket.on('message', function (data) { //When a client sents message
+        if (data.to != socket.username) //If client doens't send message to itself
+            socket.broadcast.to(users[data.to].id).emit('message', data); //Send data retrieved to client
     });
-    socket.on('messageAll', function (data) {
-        io.sockets.emit("messageAll", data);
+    socket.on('messageAll', function (data) { //When a client sents message to all chat
+        io.sockets.emit("messageAll", data); //Send data retrieved to all clients
     });
 });
 
-function updateUsers() {
-    io.sockets.emit("updateUsers", usernames);
+function updateUsers() { //Update every clients user data
+    io.sockets.emit("updateUsers", usernames); //Send data to all clients
 }
--- public/js/index.js
+++ public/js/index.js
@@ -1,198 +1,256 @@
-var socket = io();
-username = "";
-listening = "All Chat";
-users = new Array();
-focus = true;
-
-var sound = new Howl({
+socket = io(); //Inir socket io
+username = ""; //Clients username
+listening = "All Chat"; //Holds data of the open chat
+serverUsers = new Array(); //Users at the server
+users = new Array(); //All users that have been contacted
+blacklist = new Array(); //Blacklisted users for this user
+isSeen = new Array(); //The array holds data that shows if the messages have been seen
+focus = true; //Is the user look at the page at the moment
+sound = new Howl({ //Init message sound
     src: ['definite.mp3']
 });
+disconnected = false; //Is client disconnected
 
-$(document).ready(function () {
-
-    $(".btnSend").click(function () {
-        sendMessage();
+$(document).ready(function () { //Functions that called after pageload
+    $("#signConnect").hide(); //Hide connect buttın
+    $("#btnDisconnect").click(function () { //Disconnect button action
+        if (!disconnected) {
+            $("#signConnect").show();
+            $("#signDisconnect").hide();
+            socket.disconnect(); //Disconnect from socket.io server and database
+            disconnected = true;
+        } else {
+            $("#signConnect").hide();
+            $("#signDisconnect").show();
+            socket.connect(); //Connect socket.io server
+            connect(); //Register user to database
+            disconnected = false;
+        }
     });
-    $(".messageInput").keypress(function (e) {
-        var key = e.which;
+    $("#dialog-confirm").dialog({ //Init jquery modal that confirms if the user want to talk with this user
+        resizable: false,
+        height: "auto",
+        autoOpen: false,
+        width: 400,
+        modal: true,
+        show: {
+            effect: "blind",
+            duration: 1000
+        },
+        buttons: {
+            "Accept": function () {
+                $(this).dialog("close");
+            },
+            Cancel: function () {
+                blacklist[$("#senderUsernameModal").text()] = true;
+                $(this).dialog("close");
+            }
+        }
+    });
+    $(".btnSend").click(function () { //Inits user click at click
+        sendMessage(); //Sends the message 
+    });
+    $(".messageInput").keypress(function (e) { //Inits user click at enter button
+        var key = e.which; //Get data from html event
         if (key == 13) // the enter key code
         {
-            sendMessage();
+            sendMessage(); //Sends the message
             return false;
         }
     });
-    $(document).on('click', '#btnUsername', function () {
-        if ($("#usernameInput").val()) {
-            endIntro();
-            connect();
+    $(document).on('click', '#btnUsername', function () { //Set username and start the chat after connecting with click
+        if ($("#usernameInput").val()) { //Read user input
+            endIntro(); //End intro screen
+            connect(); //Login to database
         }
     });
-    $(document).on('keypress', '#usernameInput', function (e) {
+    $(document).on('keypress', '#usernameInput', function (e) { //Set username and start the chat after connecting with enter 
         if (e.which == 13) {
-            if ($("#usernameInput").val()) {
-                endIntro();
-                connect();
+            if ($("#usernameInput").val()) { //Read user input
+                endIntro(); //End intro screen
+                connect(); //Login to database
                 return false;
             }
             return false;
         }
     });
-    window.addEventListener('focus', function () {
+    window.addEventListener('focus', function () { //Init event listener that listens for users focus on the tab
         focus = true;
     });
-    window.addEventListener('blur', function () {
+    window.addEventListener('blur', function () { //Init event listener that listens for users focus on the tab
         focus = false;
     });
 });
 
-socket.on("updateUsers", function (data) {
+socket.on("updateUsers", function (data) { //Gets server updateUsers request(updates online users)
     var html = "";
+    serverUsers = data; //Get servers users and load to local data
     for (var i = 0; i < data.length; i++) {
-        if (data[i] != username)
-            html += createUser(data[i]);
+        if (data[i] != username) //If the user isn't you create a user template
+            html += createUser(data[i]); //Add to html variable
     }
-    $(".users").html(html);
-    $(".panel .info").click(function () {
-        initChangeContact($(this).children('span.userName').text());
+    $(".users").html(html); //Edit divs html
+    $(".panel .info").click(function () { //Add click function to user divs
+        initChangeContact($(this).children('span.userName').text()); //Change the focused chat window
     });
+    isSeenRefresher(); //Refresh user divs seen status
 });
 
-socket.on("message", function (data) {
-    if (!users[data.from]) {
-        users[data.from] = [];
+socket.on("message", function (data) { //Gets server message request(reterieves private message)
+    if (!blacklist[data.from]) { //Is blacklisted?
+        if (!users[data.from]) { //Is first message?
+            $("#senderUsernameModal").text(data.from); //Fill the modal window span with username of whom the message is from
+            $("#dialog-confirm").dialog("open"); //Open modal window
+            users[data.from] = []; //Create blank array node so we won't get null error
+        }
+        isSeen[data.from] = false; //Message isn't seen yet
+        users[data.from].push(data); //Push message to local array
+        if (data.from != listening || !focus) { //If when the message came user isn't focusing the tab or not looking at the senders message window
+            ringNotification(); //Ring notification sound
+        }
+        refreshMessages(); //Refresh all messages at the message window
+        isSeenRefresher(); //Refresh user divs seen status
     }
-    users[data.from].push(data);
-    if (data.from != listening || !focus) {
-        ringNotification();
-    }
-    refreshMessages();
 });
 
-socket.on("messageAll", function (data) {
-    if (!users["All Chat"]) {
-        users["All Chat"] = [];
+socket.on("messageAll", function (data) { //Gets server messageAll request(retrieves message for all chat)
+    if (!blacklist[data.from]) { //Is blacklisted?
+        if (!users["All Chat"]) { //Is first message?
+            users["All Chat"] = []; //Create blank array node so we won't get null error
+        }
+        users["All Chat"].push(data); //Push message to local array
+        if (listening != listening || !focus) { //If when the message came user isn't focusing the tab or not looking at the senders message window
+            ringNotification(); //Ring notification sound
+        }
+        refreshMessages(); //Refresh all messages at the message window
+        isSeenRefresher(); //Refresh user divs seen status
     }
-    users["All Chat"].push(data);
-    if (listening != listening || !focus) {
-        ringNotification();
-    }
-    refreshMessages();
 });
 
-function endIntro() {
-    username = $("#usernameInput").val();
-    $(".intro").fadeOut(400, function () {
-        $(".container").fadeIn(400, function () {
-            $(".container").css("display", "flex");
+
+function endIntro() { //Ends intro
+    username = $("#usernameInput").val(); //Retrieves username from input
+    $(".intro").fadeOut(400, function () { //Fade out intro screen 
+        $(".container").fadeIn(400, function () { //Fade in chat screen after intro screen
+            $(".container").css("display", "flex"); //Change display from hidden to flex
         });
     });
 }
 
-function sendMessage() {
-    to = $("#currentUserName").text();
-    message = $(".messageInput").val();
-    var d = new Date();
-    if (!message || !to) return false;
-    if (to != "All Chat") {
-        socket.emit('message', {
+function sendMessage() { //What happens when clicked send or enter
+    to = $("#currentUserName").text(); //Who is owner of the window we are looking at
+    message = $(".messageInput").val(); //What is the message we will send
+    var d = new Date(); //What is the time
+    if (!message || !to) return false; //Is message or username null or empty
+    if (to != "All Chat") { //If private
+        socket.emit('message', { //Emit data to server contains JSON
             message: message,
             to: to,
             from: username,
             time: d.getHours() + ":" + d.getMinutes()
         });
-        if (!users[to]) {
-            users[to] = [];
+        if (!users[to]) { //Is first message?
+            users[to] = []; //Create blank array node so we won't get null error
         }
-        users[to].push({
+        users[to].push({ //Save data to local Array so we can use it after a change of chat window
             from: "you",
             message: message,
             time: d.getHours() + ":" + d.getMinutes()
         });
-    } else {
-        socket.emit('messageAll', {
+    } else { //If message is to all chat
+        socket.emit('messageAll', { //Emit data to server contains JSON
             message: message,
             to: to,
             from: username,
             time: d.getHours() + ":" + d.getMinutes()
         });
     }
-    if (to != "All Chat") {
-        $(".messages").append(createMessageOut({
+    if (to != "All Chat") { //If this is a private message create a message beforehand so we will not waste servers time to send the data back to us
+        $(".messages").append(createMessageOut({ //Emit data to server contains JSON
             message: message,
             time: d.getHours() + ":" + d.getMinutes()
         }));
     }
-    $('.messages').scrollTop($('.messages')[0].scrollHeight);
-    $(".messageInput").val("");
+    $('.messages').scrollTop($('.messages')[0].scrollHeight); //Go to bottom after sending
+    $(".messageInput").val(""); //Clear message input
 }
 
-function initChangeContact(name) {
-    $("#currentUserSign").text(name.substring(0, 2));
-    $("#currentUserName").text(name);
-    listening = name;
-    refreshMessages();
+function initChangeContact(name) { //Change message window
+    if (!isSeen[name]) { //Create blank array node so we won't get null error
+        isSeen[name] = []; //Create blank array node so we won't get null error
+    }
+    isSeen[name] = true; //Message has been seen
+    $("." + name).css("color", "white"); //Change usernames color
+    $("#currentUserSign").text(name.substring(0, 2)); //Change user sign text
+    $("#currentUserName").text(name); //Change user name 
+    listening = name; //Change local variable for whose window is this
+    refreshMessages(); //Get messages from local array
 }
 
-function refreshMessages() {
-    $(".messages").empty()
-    if (!users[listening]) {
-        users[listening] = [];
+function isSeenRefresher() { //Refresh all user colors so we can see whose
+    for (var i = 0; i < serverUsers.length; i++) { //Look at the array we retrieved from server
+        if (isSeen[serverUsers[i]] == false && listening != serverUsers[i]) { //If messages are not seen and we are not looking at the window 
+            $("." + serverUsers[i]).css("color", "rgb(97, 42, 119)"); //Change user color to purple
+        }
+    }
+}
+
+function refreshMessages() { //Get all focused persons messages from local array
+    $(".messages").empty() //Empty divs html so we won't get error
+    if (!users[listening]) { //Is first message?
+        users[listening] = []; //Create blank array node so we won't get null error
     }
     html = "";
-    if (listening == "All Chat") {
-        for (var i = 0; i < users[listening].length; i++) {
+    if (listening == "All Chat") { //If we are looking at all Chat window
+        for (var i = 0; i < users[listening].length; i++) { //Retrieve all window owners messages
             if (users[listening][i].from == username) {
-                html += createMessageOut({
+                html += createMessageOut({ //Create a template for messages we send
                     message: users[listening][i].message,
                     time: users[listening][i].time
                 });
             } else {
-                html += createMessageIn({
+                html += createMessageIn({ //Create a template for messages we recieve
                     message: users[listening][i].from + " : " + users[listening][i].message,
                     time: users[listening][i].time
                 });
             }
         }
-    } else {
-        for (var i = 0; i < users[listening].length; i++) {
+    } else { //If we are looking at a private window
+        for (var i = 0; i < users[listening].length; i++) { //Retrieve all window owners messages
             if (users[listening][i].from == "you") {
-                html += createMessageOut({
+                html += createMessageOut({ //Create a template for messages we send
                     message: users[listening][i].message,
                     time: users[listening][i].time
                 });
             } else {
-                html += createMessageIn({
+                html += createMessageIn({ //Create a template for messages we recieve
                     message: users[listening][i].message,
                     time: users[listening][i].time
                 });
             }
         }
     }
-    $(".messages").html(html);
-    $(function () {
-        var wtf = $('.messages');
-        var height = $('.messages')[0].scrollHeight;
-        $('.messages').scrollTop($('.messages')[0].scrollHeight);
-    });
+    $(".messages").html(html); //Change divs html to new one
+    $('.messages').scrollTop($('.messages')[0].scrollHeight); //Go to bottom of the div
 }
 
-function connect() {
-    socket.emit('login', username);
+function connect() { //Login to server with username. NOT connecting to server
+    socket.emit('login', username); //Call servers login function
 }
 
-function createUser(name) {
-    return '<div class = "info"><span class = "userSign">' + name.substring(0, 2) + '</span> <span class = "userName" >' + name + '</span> </div>';
+function createUser(name) { //Create user template
+    return '<div class = "info"><span class = "userSign ' + name + ' ">' + name.substring(0, 2) + '</span> <span class = "userName ' + name + ' " >' + name + '</span> </div>';
 }
 
-function createMessageIn(data) {
+function createMessageIn(data) { //Create recieved message template
     return "<div class='message incoming'><p>" + data.message + "</p><span>" + data.time + "</span></div>"
 }
 
-function createMessageOut(data) {
+function createMessageOut(data) { //Create send message template
     var d = new Date();
     return "<div class='message send'><p>" + data.message + "</p><span>" + data.time + "</span></div>"
 }
 
-function ringNotification() {
+function ringNotification() { //Ring notification sound
     sound.play();
 }
